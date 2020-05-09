6ec96a0e587ce698ffeb361d2c6d1a05b7851655
Jbkey1983 <jbk_1_tn@yahoo.com>
2020-05-09 10:43:14 -0500

    ado.net

---
+++ Chinook/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Chinook.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chinook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/Customer
+        // GET: api/Customer/brazil
+        [HttpGet("{country}")]
+        public IActionResult GetByCountry(string country)
+        {
+            var repo = new CustomerRepository();
+            var customers = repo.GetByCountry(country);
+
+            if (!customers.Any())
+                return NotFound();
+
+            return Ok(customers);
+        }
+
+        // GET: api/Customer/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Customer
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Customer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
